a8a2105e085cff78a4ba4c750aaaa694df1b0beb
Jim Vaughan <jimv@mrjim.com>
2014-01-04 12:47:55 -0800

    Added Rviz_Test.c - this depends on a library in fiducials_rviz.

---
+++ fiducials/Rviz_Test.c
@@ -0,0 +1,21 @@
+
+//#include <ros/ros.h>
+
+#include "fiducials_rviz.h"
+
+int main( int argc, char** argv )
+{
+  //ros::init(argc, argv, "basic_shapes");
+  //ros::NodeHandle n;
+  //ros::Rate r(1);
+
+  void* rd = initRviz(argc, argv, "test");
+
+  //while (ros::ok())
+  while (1) 
+  {
+    sendMarker(rd, "fiducial_frame", 0, 0.0, 0.0, 0.0);
+
+    //r.sleep();
+  }
+}
