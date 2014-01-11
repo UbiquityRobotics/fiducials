6877773504d59294ee855d33caf2dd738c19e80d
trainman419 <legotown@aol.com>
2014-01-11 12:38:26 -0800

    Slowly moving to C++

--- fiducials_ros/src/fiducials_localization.cpp
+++ fiducials_ros/src/fiducials_localization.cpp
@@ -1,21 +1,28 @@
 // Copyright (c) 2013 by Wayne C. Gramlich.  All rights reserved.
 
-#include "assert.h"
-#include "sys/time.h"
+#include <assert.h>
+#include <sys/time.h>
 #include <unistd.h>
 
-#include "Character.h"
-#include "CV.h"
-#include "Double.h"
-#include "File.h"
-#include "Fiducials.h"
-#include "Float.h"
-#include "High_GUI2.h"
-#include "Integer.h"
-#include "List.h"
-#include "Logical.h"
-#include "String.h"
-#include "Unsigned.h"
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <visualization_msgs/Marker.h>
+
+#include <list>
+#include <string>
+
+#include "fiducials/CV.h"
+#include "fiducials/Character.h"
+#include "fiducials/Double.h"
+#include "fiducials/File.h"
+#include "fiducials/Fiducials.h"
+#include "fiducials/Float.h"
+#include "fiducials/High_GUI2.h"
+#include "fiducials/Integer.h"
+#include "fiducials/List.h"
+#include "fiducials/Logical.h"
+#include "fiducials/String.h"
+#include "fiducials/Unsigned.h"
 
 #include "fiducials_rviz.h"
 
@@ -33,25 +40,29 @@
 /// *Map__tag_announce*() is called each time the map algorithm
 /// updates the location or twist for a *tag*.
 
+ros::Publisher marker_pub;
+
 void Rviz__tag_announce(void *rviz, Integer id,
   Double x, Double y, Double z, Double twist, Double dx, Double dy, Double dz) {
-    File__format(stderr, "Rviz__tag_announce:id=%d x=%f y=%f twist=%f\n",
+    ROS_INFO("Rviz__tag_announce:id=%d x=%f y=%f twist=%f\n",
       id, x, y, twist);
-    Double scale = 1000.0;
+    Double scale = 100.0;
+    // TODO: publish directly to marker_pub
     sendMarker(rviz, "fiducial_frame", id, x / scale, y / scale, z / scale,
       0.0, dx / scale, dy / scale, dz / scale);
 }
 
 void Rviz__location_announce(void *rviz, Integer id,
   Double x, Double y, Double z, Double bearing) {
-    File__format(stderr, "Rviz__location_announce:id=%d x=%f y=%f bearing=%f\n",
+    ROS_INFO("Rviz__location_announce:id=%d x=%f y=%f bearing=%f\n",
       id, x, y, bearing * 180. / 3.1415926);
-    Double scale = 1000.0;
+    Double scale = 100.0;
+    // TODO: publish directly to marker_pub
     sendArrow(rviz, "fiducial_frame", id, x / scale, y / scale, z / scale,
         0.0, .2, .05, .05, bearing);
 }
 
-int main(int arguments_size, char * arguments[]) {
+int main(int argc, char ** argv) {
     struct timeval start_time_value_struct;    
     struct timeval end_time_value_struct;    
     struct timeval difference_time_value_struct;    
@@ -59,53 +70,38 @@ int main(int arguments_size, char * arguments[]) {
     Time_Value end_time_value = &end_time_value_struct;
     Time_Value difference_time_value = &difference_time_value_struct;
 
-    assert (gettimeofday(start_time_value, (struct timezone *)0) == 0);
+    ros::init(argc, argv, "fiducials_localization");
+    ros::NodeHandle nh("~");
 
-    List /* <String> */ image_file_names = List__new();
-    String lens_calibrate_file_name = (String)0;
-    //File__format(stdout, "Hello\n");
-    if (arguments_size <= 1) {
-        File__format(stderr, "Usage: Demo lens.txt *.pnm\n");
-    } else {
-        for (Unsigned index = 1; index < arguments_size; index++) {
-            String argument = arguments[index];
-            Unsigned size = String__size(argument);
-            if (size > 4 && String__equal(argument + size - 4, ".txt")) {
-                lens_calibrate_file_name = argument;
-            } else if (size > 4 && String__equal(argument + size - 4, ".pnm")) {
-                List__append(image_file_names, argument);
-            } else {
-                File__format(stderr, "Unrecoginized file '%s'\n", argument);
-            }
-        }
+    assert(gettimeofday(start_time_value, (struct timezone *)0) == 0);
+
+    std::list<std::string> image_file_names;
+    std::string lens_calibration_file;
+
+    nh.getParam("lens_calibration", lens_calibration_file);
+
+    for( int i=0; i<argc; ++i ) {
+      image_file_names.push_back(argv[i]);
+      ROS_INFO("Image file: %s", argv[i]);
     }
 
-    Unsigned size = List__size(image_file_names);
+    int size = image_file_names.size();
     if (size > 0) {
-        String image_file_name0 = (String)List__fetch(image_file_names, 0);
+        std::string image_file_name0 = image_file_names.front();
         CV_Image image = (CV_Image)0;
-        image = CV_Image__pnm_read(image_file_name0);
+        image = CV_Image__pnm_read(image_file_name0.c_str());
         assert (image != (CV_Image)0);
-        void *rviz = initRviz(arguments_size, arguments, "Rviz_Demo");
         Fiducials fiducials =
-          Fiducials__create(image, lens_calibrate_file_name,
-          rviz, Rviz__location_announce, Rviz__tag_announce);
+          Fiducials__create(image, lens_calibration_file.c_str(),
+          NULL, Rviz__location_announce, Rviz__tag_announce);
         Fiducials__tag_heights_xml_read(fiducials, "Tag_Heights.xml");
 
-        for (Unsigned index = 0; index < size; index++) {
-            String image_file_name = 
-              (String)List__fetch(image_file_names, index);
-            image = CV_Image__pnm_read(image_file_name);
+        for( std::list<std::string>::const_iterator itr = 
+            image_file_names.begin() ; itr != image_file_names.end(); ++itr) {
+            image = CV_Image__pnm_read(itr->c_str());
             Fiducials__image_set(fiducials, image);
             Fiducials__process(fiducials);
             sleep(2);
-
-            //float xpos = 0.0;
-            //float ypos = 0.0;
-            //float zpos = 0.0;
-            //int id = 0;
-            //File__format(stderr, "sent a frame\n");
-            //sendMarker(rviz, "fiducial_frame", id, xpos, ypos, zpos);
         }
 
         assert (gettimeofday(end_time_value, (struct timezone *)0) == 0);
@@ -117,8 +113,8 @@ int main(int arguments_size, char * arguments[]) {
         Double time = end_time - start_time;
         Double frames_per_second = (Double)size / time;
 
-        File__format(stderr, "%d frames / %f sec = %f Frame/sec\n",
-          size, time, frames_per_second);
+        ROS_INFO("%d frames / %f sec = %f Frame/sec\n", size, time,
+            frames_per_second);
 
         if (size == 1) {
             Fiducials__image_show(fiducials, (Logical)1);
@@ -126,8 +122,7 @@ int main(int arguments_size, char * arguments[]) {
             Map map = fiducials->map;
             Map__save(map, "Rviz_Demo.xml");
             List /*<Location>*/ locations = fiducials->locations;
-            File__format(stderr,
-              "Outputing %d locations\n", List__size(locations));
+            ROS_INFO("Outputing %d locations\n", List__size(locations));
             Map__svg_write(map, "Demo", locations);
         }
     }
