c1543163efe60eaf5f496eb824bb194a776569d8
Jim Vaughan <jimv@mrjim.com>
2015-07-10 14:08:49 -0700

    removed superfluous include

--- fiducial_pose/src/calcPose.cpp
+++ fiducial_pose/src/calcPose.cpp
@@ -12,7 +12,6 @@ the fiducial relative to a fiducial centered on the origin.
 
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-//#include <turtlesim/Pose.h>
 #include <tf/LinearMath/Matrix3x3.h>
 
 #include <opencv2/opencv.hpp>
