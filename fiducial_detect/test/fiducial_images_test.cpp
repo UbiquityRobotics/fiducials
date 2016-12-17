34d45e3c408f3733e898ef1cad0fece0247324b6
Rohan Agrawal <send2arohan@gmail.com>
2016-12-17 15:54:03 -0800

    Get rid of old test code

--- fiducial_detect/test/fiducial_images_test.cpp
+++ fiducial_detect/test/fiducial_images_test.cpp
@@ -134,19 +134,4 @@ int main(int argc, char** argv)
   testing::InitGoogleTest(&argc, argv);
   ros::init(argc, argv, "FiducialImagesTest");
   return RUN_ALL_TESTS();
-
-//   ros::init(argc, argv, "image_publisher");
-//   ros::NodeHandle nh;
-//   image_transport::ImageTransport it(nh);
-//   image_transport::Publisher pub = it.advertise("camera/image", 1);
-//   cv::Mat image = cv::imread("/home/rohan/kinetic_catkin/src/fiducials/fiducial_detect/test/test_images/fiducial_30.png", CV_LOAD_IMAGE_COLOR);
-//   cv::waitKey(30);
-//   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-
-//   ros::Rate loop_rate(5);
-//   while (nh.ok()) {
-//     pub.publish(msg);
-//     ros::spinOnce();
-//     loop_rate.sleep();
-//   }
 }
