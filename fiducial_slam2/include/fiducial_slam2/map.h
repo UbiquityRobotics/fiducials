242266fb2403d4d09b6033ca5c0b831b69ede463
Jim Vaughan <jimv@mrjim.com>
2017-04-02 13:50:33 -0700

    Added fiducial_slam2 - c++ slam node

---
+++ fiducial_slam2/include/fiducial_slam2/map.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2017, Ubiquity Robotics
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the FreeBSD Project.
+ *
+ */
+
+#include <ros/ros.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <visualization_msgs/Marker.h>
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/calib3d.hpp>
+
+#include <fiducial_msgs/FiducialMapEntry.h>
+#include <fiducial_msgs/FiducialMapEntryArray.h>
+
+#include <list>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+// An observation of a single fiducial in a single image
+class Observation {
+  public:
+    int fid;
+    double imageError;
+    double objectError;
+    tf2::Transform TfidCam;
+    tf2::Transform TcamFid;
+  
+    Observation(int fid, const cv::Vec3d &rvec, const cv::Vec3d &tvec,
+                double ierr, double oerr);
+    Observation(int fid, const tf2::Quaternion &q, const tf2::Vector3 &tvec,
+                double ierr, double oerr);
+};
+
+// A single fiducial that is in the map
+class Fiducial {
+  public:
+    int id;
+    tf2::Transform pose;
+    double variance;
+    ros::Time lastPublished;
+    bool anchor;
+
+    void update(const tf2::Transform &pose, double variance);
+
+    Fiducial() {}
+
+    Fiducial(int id, const tf2::Transform &T, double variance);
+    Fiducial(int id, const tf2::Quaternion &q, const tf2::Vector3 &tvec,
+             double variance, bool anchor);
+};
+
+// Class containing map data
+class Map {
+  public:
+    tf2_ros::TransformBroadcaster broadcaster;
+    tf2_ros::Buffer *tfBuffer;
+    tf2_ros::TransformListener *listener;
+
+    ros::Publisher *markerPub;
+    ros::Publisher *mapPub;
+    string filename;
+
+    map<int, Fiducial> fiducials;
+
+    Map(ros::NodeHandle &nh);
+    void update(const vector<Observation> &obs, ros::Time time);
+    void autoInit(const vector<Observation> &obs, ros::Time time);
+    void updateMap(const vector<Observation> &obs, ros::Time time);
+    void updatePose(const vector<Observation> &obs, ros::Time time);
+    bool load();
+    bool save();
+    void publishMap();
+    void publishMarker(Fiducial &fid);
+    void publishMarkers();
+};
+
---
+++ fiducial_slam2/src/fiducial_slam.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2017, Ubiquity Robotics
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the FreeBSD Project.
+ *
+ */
+
+#include <assert.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <visualization_msgs/Marker.h>
+
+#include "fiducial_msgs/Fiducial.h"
+#include "fiducial_msgs/FiducialArray.h"
+#include "fiducial_msgs/FiducialTransform.h"
+#include "fiducial_msgs/FiducialTransformArray.h"
+
+#include "fiducial_slam2/map.h"
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+
+
+#include <list>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+
+class FiducialSlam {
+  private:
+    ros::Subscriber ft_sub;
+
+    void transformCallback(const fiducial_msgs::FiducialTransformArray::ConstPtr &msg);
+
+  public:
+    Map *fiducialMap;
+    FiducialSlam(ros::NodeHandle &nh);
+};
+
+
+void FiducialSlam::transformCallback(const fiducial_msgs::FiducialTransformArray::ConstPtr& msg)
+{
+ 
+    vector<Observation> observations;
+
+    for (int i=0; i<msg->transforms.size(); i++) {
+        const fiducial_msgs::FiducialTransform &ft = msg->transforms[i];
+
+        tf2::Vector3 tvec(ft.transform.translation.x,
+                          ft.transform.translation.y,
+                          ft.transform.translation.z);
+ 
+        tf2::Quaternion q(ft.transform.rotation.x,
+                          ft.transform.rotation.y,
+                          ft.transform.rotation.z,
+                          ft.transform.rotation.w);
+         
+        Observation obs(ft.fiducial_id, q, tvec, ft.image_error,
+                        ft.object_error); 
+        observations.push_back(obs);
+    }
+
+    fiducialMap->update(observations, msg->header.stamp);
+}
+
+FiducialSlam::FiducialSlam(ros::NodeHandle & nh)
+{
+    fiducialMap = new Map(nh);
+    fiducialMap->load();
+
+    ft_sub = nh.subscribe("/fiducial_transforms", 1, 
+                          &FiducialSlam::transformCallback, this); 
+    
+    ROS_INFO("Fiducial Slam ready");
+}
+
+FiducialSlam *node = NULL;
+
+void mySigintHandler(int sig)
+{
+    if (node)
+        node->fiducialMap->save();
+
+    ros::shutdown();
+}
+
+int main(int argc, char ** argv) {
+    ros::init(argc, argv, "fiducial_slam", ros::init_options::NoSigintHandler);
+    ros::NodeHandle nh("~");
+
+    node = new FiducialSlam(nh);
+    signal(SIGINT, mySigintHandler);
+
+    ros::Rate r(10);
+    while (ros::ok()) {
+        r.sleep();
+        ros::spinOnce(); 
+        node->fiducialMap->publishMarkers();
+    }
+
+    return 0;
+}
