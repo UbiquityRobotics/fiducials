0037fba6bfc91153bfeec2ff1b870ceaf6a9324f
Rohan Agrawal <send2arohan@gmail.com>
2018-10-06 20:37:35 -0700

    use empy to calculate svg positions (dynamically)

--- aruco_detect/scripts/create_markers.py
+++ aruco_detect/scripts/create_markers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os, sys
+import em
 
 """
 Generate a PDF file containaing one or more fiducial marker for printing
@@ -15,36 +16,48 @@ def checkCmd(cmd, package):
      
 def genSvg(file, id, dicno):
     f = open(file, "w")
-    f.write("""<svg width="208.0mm" height="240.0mm"
+    f.write(em.expand("""<svg width="@(paper_width)mm" height="@(paper_height)mm"
  version="1.1"
  xmlns:xlink="http://www.w3.org/1999/xlink"
  xmlns="http://www.w3.org/2000/svg">
 
-  <line x1="5.0mm" y1="5.0mm" x2="7.0mm" y2="5.0mm" style="stroke:black"/>
-  <line x1="195.0mm" y1="5.0mm" x2="197.0mm" y2="5.0mm" style="stroke:black"/>
-  <line x1="5.0mm" y1="21.0mm" x2="7.0mm" y2="21.0mm" style="stroke:black"/>
-  <line x1="5.0mm" y1="21.0mm" x2="5.0mm" y2="23.0mm" style="stroke:black"/>
-  <line x1="197.0mm" y1="21.0mm" x2="195.0mm" y2="21.0mm" style="stroke:black"/>
-  <line x1="197.0mm" y1="21.0mm" x2="197.0mm" y2="23.0mm" style="stroke:black"/>
+  <rect x="@((paper_width - fid_len)/2)mm" y="@((paper_height - fid_len)/2)mm" width="@(fid_len)mm" height="4.0mm" style="stroke:none; fill:black"/>
+  <rect x="@((paper_width - fid_len)/2)mm" y="@((paper_height + fid_len)/2 - 4)mm" width="@(fid_len)mm" height="4.0mm" style="stroke:none; fill:black"/>
+  <rect x="@((paper_width - fid_len)/2)mm" y="@((paper_height - fid_len)/2)mm" width="4.0mm" height="@(fid_len)mm" style="stroke:none; fill:black"/>
+  <rect x="@((paper_width + fid_len)/2 - 4)mm" y="@((paper_height - fid_len)/2)mm" width="4.0mm" height="@(fid_len)mm" style="stroke:none; fill:black"/>
+
+  <image x="@((paper_width - fid_len)/2)mm" y="@((paper_height - fid_len)/2)mm" width="@(fid_len)mm" height="@(fid_len)mm" xlink:href="marker@(id).png" />
+
+
+  @{cut = 15}
+
+  @{corner_x = (paper_width - fid_len)/2 - cut}
+  @{corner_y = (paper_height - fid_len)/2 - cut}
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x + 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y + 2)mm" style="stroke:black"/>
   
-  <image x="31.0mm" y="47.0mm" width="140.0mm" height="140.0mm" xlink:href="marker%d.png" />
+  <text x="@(paper_width/2)mm" y="@(corner_y - 2)mm" text-anchor="middle" style="font-family:ariel; font-size:12;">1 cm</text>
+  <line x1="@(paper_width/2 - 5)mm" y1="@(corner_y)mm" x2="@(paper_width/2 + 5)mm" y2="@(corner_y)mm" style="stroke:black"/>
 
-  <rect x="31.0mm" y="47.0mm" width="140.0mm" height="4.0mm" style="stroke:black; fill:black"/>
-  <rect x="31.0mm" y="183.0mm" width="140.0mm" height="4.0mm" style="stroke:black; fill:black"/>
-  <rect x="31.0mm" y="47.0mm" width="4.0mm" height="140.0mm" style="stroke:black; fill:black"/>
-  <rect x="167.0mm" y="47.0mm" width="4.0mm" height="140.0mm" style="stroke:black; fill:black"/>
+  @{corner_x = (paper_width + fid_len)/2 + cut}
+  @{corner_y = (paper_height - fid_len)/2 - cut}
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x - 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y + 2)mm" style="stroke:black"/>
 
-  <line x1="5.0mm" y1="213.0mm" x2="7.0mm" y2="213.0mm" style="stroke:black"/>
-  <line x1="5.0mm" y1="213.0mm" x2="5.0mm" y2="211.0mm" style="stroke:black"/>
-  <line x1="195.0mm" y1="213.0mm" x2="197.0mm" y2="213.0mm" style="stroke:black"/>
-  <line x1="197.0mm" y1="213.0mm" x2="197.0mm" y2="211.0mm" style="stroke:black"/>
-  <line x1="5.0mm" y1="229.0mm" x2="7.0mm" y2="229.0mm" style="stroke:black"/>
-  <line x1="195.0mm" y1="229.0mm" x2="197.0mm" y2="229.0mm" style="stroke:black"/>
+  @{corner_x = (paper_width - fid_len)/2 - cut}
+  @{corner_y = (paper_height + fid_len)/2 + cut}
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x + 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y - 2)mm" style="stroke:black"/>
 
-  <text x="90.0mm" y="220.0mm" style="font-family:ariel; font-size:24">%d D%d</text>
+  @{corner_x = (paper_width + fid_len)/2 + cut}
+  @{corner_y = (paper_height + fid_len)/2 + cut}
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x - 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
+  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y - 2)mm" style="stroke:black"/>
+
+  <text x="@(paper_width/2)mm" y="220.0mm" text-anchor="middle" style="font-family:ariel; font-size:24;">@(id) D@(dicno)</text>
 
 </svg>
-""" % (id, id, dicno))
+""", {"id": id, "dicno": dicno, "paper_width": 215.9, "paper_height": 279.4, "fid_len": 140.0}))
     f.close()
 
 if __name__ == "__main__":
@@ -67,9 +80,9 @@ if __name__ == "__main__":
         sys.stdout.flush()
         os.system("rosrun aruco_detect create_marker --id=%d --ms=2000 --d=%d marker%d.png" % (i, dicno, i))
         genSvg("marker%d.svg" % i, i, 7)
-        os.system("inkscape --without-gui --export-pdf=marker%d.pdf marker%d.svg" % (i, i))
-        os.remove("marker%d.svg" % i)
-        os.remove("marker%d.png" % i)
+        os.system("inkscape --without-gui --export-area-page --export-pdf=marker%d.pdf marker%d.svg" % (i, i))
+#        os.remove("marker%d.svg" % i)
+#        os.remove("marker%d.png" % i)
     print "Combining into %s" % outfile
     os.system("pdfunite %s %s" % (" ".join(pdfs), outfile))
     for f in pdfs:
