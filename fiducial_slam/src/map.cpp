8577c790805992896015a3523d3fa527fc1f06e1
David Crawley <davecrawley@gmail.com>
2019-03-26 08:44:42 -0700

    WIP fixed incorrect extraction of mean1

--- fiducial_slam/src/map.cpp
+++ fiducial_slam/src/map.cpp
@@ -73,9 +73,13 @@ static double updateVarianceDavid(const tf2::Vector3 &newMean,
     if (sum_error_in_quadrature) {                          //Shouldn't it be eliminated?
        return suminquadrature(var1, var2);                  //Shouldn't it be eliminated?
     }                                                       //Shouldn't it be eliminated?
+    
+    double meanfunc1 = mean1.length2();  //Is this the correct extraction of the mean from the 1st input? We can clean this line out and incorporate in to line below if we are sure
+    double meanfunc2 = mean2.length2();  //Is this the correct extraction of the mean from the 2nd input? We can clean this line out if we are sure
+    
     double var1_w_syst_err = suminquadrature(var1,systematic_error);    //adding in the systematic error **before** the variances get used for anything
     double var2_w_syst_err = suminquadrature(var2,systematic_error);    //adding in the systematic error **before** the variances get used for anything
-    double est_prob_dens_at_newvar = suminquadrature(pdf(var1_w_syst_err,mean1,newMean),pdf(var2_w_syst_err, mean2, newMean));  // This returns the estimated probability density given the probability density that would be expected from the two measurements at the most likely point where it will be 
+    double est_prob_dens_at_newvar = suminquadrature(pdf(var1_w_syst_err,meanfunc1,newMean),pdf(var2_w_syst_err, meanfunc2, newMean));  // This returns the estimated probability density given the probability density that would be expected from the two measurements at the most likely point where it will be 
     double newVar = (1.0/(est_prob_dens_at_newvar*( sqrt (2.0 *M_PI)))); //this converts this estimated probability density in to the new variance
 
     if (newVar > 100)    //If this occurs then there is something seriously wrong - its not clear to me that this is helpful and may just hide problems
