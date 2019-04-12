62b4e4d70de6abeac33c9f4096046124e5458629
Rohan Agrawal <send2arohan@gmail.com>
2019-04-11 22:25:16 -0700

    test fusion of bad data

--- fiducial_slam/test/transform_var_test.cpp
+++ fiducial_slam/test/transform_var_test.cpp
@@ -48,8 +48,52 @@ TEST (TransformWithVariance, same_fusion_iterative) {
 
        // The new variance should be less than the origial, but non negative
        ASSERT_LT(out_tv.variance, 0.3);
-       ASSERT_GT(out_tv.variance, 0);
+       ASSERT_GT(out_tv.variance, 1e-9);
     }
+}
+
+TEST (TransformWithVariance, outlier_with_large_variance) {
+    auto t1 = tf2::Transform(tf2::Quaternion(0,0,0,1), tf2::Vector3(0,0,0));
+    auto t2 = tf2::Transform(tf2::Quaternion(0,0,0,1), tf2::Vector3(0.1,0,0));
+    auto t3 = tf2::Transform(tf2::Quaternion(0,0,0,1), tf2::Vector3(0.1,0,0));
+    auto t4 = tf2::Transform(tf2::Quaternion(0,0,0,1), tf2::Vector3(1.0,0,0));
+
+    auto tv1 = TransformWithVariance(t1, 0.2);
+    auto tv2 = TransformWithVariance(t2, 0.2);
+    auto tv3 = TransformWithVariance(t3, 0.2);
+    auto tv4 = TransformWithVariance(t4, 2.0);
+
+    auto out_tv = averageTransforms(tv1, tv2);
+    out_tv = averageTransforms(out_tv, tv3);
+    out_tv = averageTransforms(out_tv, tv4);
+
+    // Make sure that the new position is between the originals 
+    ASSERT_GT(out_tv.transform.getOrigin().x(), 0); 
+    ASSERT_LT(out_tv.transform.getOrigin().x(), 1.0);
+
+    // The new variance shouldn't be large
+    ASSERT_LT(out_tv.variance, 1.0);
+    ASSERT_GT(out_tv.variance, 0);
+
+    // The new mean shouldn't be affected much by the outlier
+    ASSERT_NEAR(out_tv.transform.getOrigin().x(), 0.1, 0.05);
+}
+
+TEST (TransformWithVariance, different_with_similar_variance) {
+    auto t1 = tf2::Transform(tf2::Quaternion(0,0,0,1), tf2::Vector3(0,0,0));
+    auto t2 = tf2::Transform(tf2::Quaternion(0,0,0,1), tf2::Vector3(1.0,0,0));
+
+    auto tv1 = TransformWithVariance(t1, 0.1);
+    auto tv2 = TransformWithVariance(t2, 0.2);
+
+    auto out_tv = averageTransforms(tv1, tv2);
+
+    // Make sure that the new position is between the originals 
+    ASSERT_GT(out_tv.transform.getOrigin().x(), 0); 
+    ASSERT_LT(out_tv.transform.getOrigin().x(), 1.0);
+
+    // The new variance should be larger because of disagreeing data
+    ASSERT_GT(out_tv.variance, 0.2);
 }
 
 int main(int argc, char **argv) {
