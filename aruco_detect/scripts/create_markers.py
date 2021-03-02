22ecfa87e4f01bbe699be5d085818720b06c7ea2
Ajith Thomas <ajiththomas152@gmail.com>
2021-03-02 19:01:03 +0530

    Added argument to change fiducial length and scaling the marker based on it

--- aruco_detect/scripts/create_markers.py
+++ aruco_detect/scripts/create_markers.py
@@ -34,7 +34,7 @@ def checkCmd(cmd, package):
     sudo apt install %s""" % (cmd, package))
         sys.exit(1)
      
-def genSvg(id, dicno, paper_size):
+def genSvg(id, dicno, paper_size, fid_len):
     return em.expand("""<svg width="@(paper_width)mm" height="@(paper_height)mm"
  version="1.1"
  xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -82,15 +82,15 @@ def genSvg(id, dicno, paper_size):
   <text x="@(paper_width/2)mm" y="@((paper_height + fid_len)/2 + 30)mm" text-anchor="middle" style="font-family:ariel; font-size:24;">@(id) D@(dicno)</text>
 
 </svg>
-""", {"id": id, "dicno": dicno, "paper_width": paper_size[0], "paper_height": paper_size[1], "fid_len": 140.0})
+""", {"id": id, "dicno": dicno, "paper_width": paper_size[0], "paper_height": paper_size[1], "fid_len": (fid_len*10.0)})
 
-def genMarker(i, dicno, paper_size):
+def genMarker(i, dicno, paper_size, fid_len):
     print(" Marker %d\r" % i)
     sys.stdout.flush()
     aruco_dict = aruco.Dictionary_get(dicno)
-    img = aruco.drawMarker(aruco_dict, i, 2000)
+    img = aruco.drawMarker(aruco_dict, i, int(2000 * (fid_len/14.0)))
     cv2.imwrite("/tmp/marker%d.png" % i, img)
-    svg = genSvg(i, dicno, paper_size)
+    svg = genSvg(i, dicno, paper_size, fid_len)
     cairosvg.svg2pdf(bytestring=svg, write_to='/tmp/marker%d.pdf' % i)
     # Old slower method using subprocess for SVG to PDF conversion
     # cairo = subprocess.Popen(('cairosvg', '-f', 'pdf', '-o', '/tmp/marker%d.pdf' % i, '/dev/stdin'), stdin=subprocess.PIPE)
@@ -113,6 +113,8 @@ if __name__ == "__main__":
                         help='dictionary to generate from')
     parser.add_argument('--paper-size', dest='paper_size', action='store',
                         default='letter', help='paper size to use (letter or a4)')
+    parser.add_argument('--fid-len', type=float, dest='fid_len', action="store",
+                        default="14.0", help="fiducial side length in centimeter")
 
     args = parser.parse_args()
 
@@ -127,20 +129,22 @@ if __name__ == "__main__":
     elif args.paper_size == 'a4':
         paper_size  = (210, 297)
 
+    fid_len = args.fid_len
+
     try:
         # For a parallel version
         from joblib import Parallel, delayed
-        Parallel(n_jobs=-1)(delayed(genMarker)(i, dicno, paper_size) for i in markers)
+        Parallel(n_jobs=-1)(delayed(genMarker)(i, dicno, paper_size, fid_len) for i in markers)
     except ImportError:
         # Fallback to serial version
         for i in markers:
-            genMarker(i, dicno, paper_size)
+            genMarker(i, dicno, paper_size, fid_len)
 
     print("Combining into %s" % outfile)
     os.system("pdfunite %s %s" % (" ".join(pdfs), outfile))
     for f in pdfs:
         os.remove(f)
 
-    print('\033[91m' + """After printing, please make sure that the long lines around the marker are
-EXACTLY 14.0 cm long. This is required for accurate position estimation.""" + '\033[0m')
+    print('\033[91m' + """After printing, please make sure that the long lines around the marker are 
+          EXACTLY %3.1f cm long. This is required for accurate position estimation."""  % fid_len + '\033[0m')
 
