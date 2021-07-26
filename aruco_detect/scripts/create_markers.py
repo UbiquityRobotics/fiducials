a00831ce5b12e69e29a2fec8283a05320051e687
Rohan Agrawal <ra@ubiquityrobotics.com>
2021-07-26 10:40:11 -0700

    Updated marker generation script to work with Python3  (#254)
    * Updated create marker script for python3
    
    * Updated to use Python 3 cairosvg module
    
    * Added argument to change fiducial length and scaling the marker based on it
    
    * Change to Python 3
    
    * Fixed string format
    
    * Changed argument name for passing in fiducial length
    
    * Updated marker generation script to work with Python3
    
    * Added a comment about why marker_gen is a separate module
    
    * gitignore bagfiles, we don't want them to be checked in generally
    
    Co-authored-by: Ajith Thomas <ajiththomas152@gmail.com>

--- aruco_detect/scripts/create_markers.py
+++ aruco_detect/scripts/create_markers.py
@@ -1,104 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+"""
+Generate a PDF file containaing one or more fiducial markers for printing
+"""
 
 import os, sys, argparse
 import subprocess
-import imp, importlib
-
-# Hack if a user has 'em' instead of 'empy' installed with pip
-# If the module em doesn't have the expand function use the full path
-em = None
-for path in sys.path:
-    filename = os.path.join(path, 'em.py')
-    if os.path.exists(filename):
-         em = imp.load_source('em', filename)
-         if "expand" in dir(em):
-             break
-# For-else: else is called if loop doesn't break 
-else:
-    print "ERROR: could not find module em, please sudo apt install python-empy"
-    exit(2)
-
-import cv2
-import cv2.aruco as aruco
-
-"""
-Generate a PDF file containaing one or more fiducial marker for printing
-"""
+from marker_generation import genMarker
 
 def checkCmd(cmd, package):
     rc = os.system("which %s > /dev/null" % cmd)
     if rc != 0:
-        print """This utility requires %s. It can be installed by typing:
-    sudo apt install %s""" % (cmd, package)
+        print("""This utility requires %s. It can be installed by typing:
+    sudo apt install %s""" % (cmd, package))
         sys.exit(1)
      
-def genSvg(id, dicno, paper_size):
-    return em.expand("""<svg width="@(paper_width)mm" height="@(paper_height)mm"
- version="1.1"
- xmlns:xlink="http://www.w3.org/1999/xlink"
- xmlns="http://www.w3.org/2000/svg">
-
-  <rect x="@((paper_width - fid_len)/2)mm" y="@((paper_height - fid_len)/2)mm" width="@(fid_len)mm" height="4.0mm" style="stroke:none; fill:black"/>
-  <rect x="@((paper_width - fid_len)/2)mm" y="@((paper_height + fid_len)/2 - 4)mm" width="@(fid_len)mm" height="4.0mm" style="stroke:none; fill:black"/>
-  <rect x="@((paper_width - fid_len)/2)mm" y="@((paper_height - fid_len)/2)mm" width="4.0mm" height="@(fid_len)mm" style="stroke:none; fill:black"/>
-  <rect x="@((paper_width + fid_len)/2 - 4)mm" y="@((paper_height - fid_len)/2)mm" width="4.0mm" height="@(fid_len)mm" style="stroke:none; fill:black"/>
-
-  <image x="@((paper_width - fid_len)/2)mm" y="@((paper_height - fid_len)/2)mm" width="@(fid_len)mm" height="@(fid_len)mm" xlink:href="/tmp/marker@(id).png" />
-
-
-  @{cut = max(fid_len/10 * 1.4, 10)}
-
-  @{corner_x = (paper_width - fid_len)/2 - cut}
-  @{corner_y = (paper_height - fid_len)/2 - cut}
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x + 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y + 2)mm" style="stroke:black"/>
-  
-  @{corner_x = (paper_width + fid_len)/2 + cut}
-  @{corner_y = (paper_height - fid_len)/2 - cut}
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x - 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y + 2)mm" style="stroke:black"/>
-
-  <text x="@(paper_width/2)mm" y="@(corner_y - 1)mm" text-anchor="middle" style="font-family:ariel; font-size:8;">
-      This line should be exactly @(fid_len/10)cm long. 
-  </text>
-  <line x1="@(paper_width/2 - fid_len/2)mm" y1="@(corner_y)mm" x2="@(paper_width/2 + fid_len/2)mm" y2="@(corner_y)mm" style="stroke:black"/>
-  <line x1="@(corner_x)mm" y1="@(paper_height/2 - fid_len/2)mm" x2="@(corner_x)mm" y2="@(paper_height/2 + fid_len/2)mm" style="stroke:black"/>
-
-  @{corner_x = (paper_width - fid_len)/2 - cut}
-  @{corner_y = (paper_height + fid_len)/2 + cut}
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x + 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y - 2)mm" style="stroke:black"/>
-
-  <line x1="@(corner_x)mm" y1="@(paper_height/2 - fid_len/2)mm" x2="@(corner_x)mm" y2="@(paper_height/2 + fid_len/2)mm" style="stroke:black"/>
-  <line x1="@(paper_width/2 - fid_len/2)mm" y1="@(corner_y)mm" x2="@(paper_width/2 + fid_len/2)mm" y2="@(corner_y)mm" style="stroke:black"/>
-
-  @{corner_x = (paper_width + fid_len)/2 + cut}
-  @{corner_y = (paper_height + fid_len)/2 + cut}
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x - 2)mm" y2="@(corner_y)mm" style="stroke:black"/>
-  <line x1="@(corner_x)mm" y1="@(corner_y)mm" x2="@(corner_x)mm" y2="@(corner_y - 2)mm" style="stroke:black"/>
-
-  <text x="@(paper_width/2)mm" y="@((paper_height + fid_len)/2 + 30)mm" text-anchor="middle" style="font-family:ariel; font-size:24;">@(id) D@(dicno)</text>
-
-</svg>
-""", {"id": id, "dicno": dicno, "paper_width": paper_size[0], "paper_height": paper_size[1], "fid_len": 140.0})
-
-def genMarker(i, dicno, paper_size):
-    print " Marker %d\r" % i,
-    sys.stdout.flush()
-    aruco_dict = aruco.Dictionary_get(dicno)
-    img = aruco.drawMarker(aruco_dict, i, 2000)
-    cv2.imwrite("/tmp/marker%d.png" % i, img)
-    svg = genSvg(i, dicno, paper_size)
-    cairo = subprocess.Popen(('cairosvg', '-f', 'pdf', '-o', '/tmp/marker%d.pdf' % i, '/dev/stdin'), stdin=subprocess.PIPE)
-    cairo.communicate(input=svg)
-    # This way is faster than subprocess, but causes problems when cairosvg is installed from pip
-    # because newer versions only support python3, and opencv3 from ros does not
-    # cairosvg.svg2pdf(bytestring=svg, write_to='/tmp/marker%d.pdf' % i)
-    os.remove("/tmp/marker%d.png" % i)
-
 if __name__ == "__main__":
     checkCmd("pdfunite", "poppler-utils")
-    checkCmd("cairosvg", "python-cairosvg")
+    checkCmd("cairosvg", "cairosvg python3-cairosvg")
 
 
     parser = argparse.ArgumentParser(description='Generate Aruco Markers.')
@@ -112,7 +31,6 @@ if __name__ == "__main__":
                         help='dictionary to generate from')
     parser.add_argument('--paper-size', dest='paper_size', action='store',
                         default='letter', help='paper size to use (letter or a4)')
-
     args = parser.parse_args()
 
     outfile = args.pdfFile
@@ -135,11 +53,11 @@ if __name__ == "__main__":
         for i in markers:
             genMarker(i, dicno, paper_size)
 
-    print "Combining into %s" % outfile
+    print("Combining into %s" % outfile)
     os.system("pdfunite %s %s" % (" ".join(pdfs), outfile))
     for f in pdfs:
         os.remove(f)
 
-    print '\033[91m' + """After printing, please make sure that the long lines around the marker are
-EXACTLY 14.0 cm long. This is required for accurate position estimation.""" + '\033[0m'
+    print('\033[91m' + """After printing, please make sure that the long lines around the marker are 
+EXACTLY 14.0cm long. This is required for accurate position estimation.""" + '\033[0m')
 
